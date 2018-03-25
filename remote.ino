6b90b43100756a69b7ee6c4773860682e27148fb
anilamatcha <anilamatcha@gmail.com>
2018-03-25 17:43:57 +0530

    Add files via upload

---
+++ remote.ino
@@ -0,0 +1,25 @@
+
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "cad789dea7384e30b78db4922db44cc9";
+
+char ssid[] = "anila";
+char pass[] = "12345678";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
+
